8628b0824f6c0eb8073ca5ab253cd7a34c054597
FredPeal <frederickpeal@gmail.com>
2021-10-30 13:43:55 -0400

    Migrations for users

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Roles;
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Roles::insert([
+            [
+                'name' => 'Admin'
+            ],
+            [
+                'name' => 'Owner'
+            ],
+            [
+                'name' => 'User'
+            ]
+        ]);
+    }
+}
